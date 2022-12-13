492c1a4e0451814640368fc2bc67af71bc6dbcf7
BittingBarracuda <kmartinez2397@gmail.com>
2022-12-13 13:01:11 +0100

    Added comments with matrix dimensions to gradient descent methods

--- pmf.py
+++ pmf.py
@@ -33,15 +33,31 @@ class ProbabilisticMatrixFactorization:
             print(f'Epoch-{t}')          
     
     def __get_U_gradient(self, i):
+        # I_ij -> 1 x M
         I_ij = np.copy(self.R[i, :])
         I_ij[np.where(I_ij > 0)] = 1
+        # self.R[i, :] -> 1 x M
+        # self.U[i, :] -> 1 x D
+        # self.V -> D x M
+        # np.dot(self.U[i, :], self.V) -> 1 x M
+        # aux_1 -> 1 x M
+        # aux_2 -> D x M 
+        # sum(aux_2, axis = 1) -> D x 1
         aux_1 = I_ij * (self.R[i, :] - np.dot(self.U[i, :], self.V))
         aux_2 = aux_1 * self.V
         return (1 / self.sigma) * np.sum(aux_2, axis=1) + (self.U[i, :] / self.sigma_u)
 
     def __get_V_gradient(self, j):
+        # I_ij -> 1 x M
         I_ij = np.copy(self.R[:, j])
         I_ij[np.where(I_ij) > 0] = 1
+        # self.R[:, j] -> 1 x N
+        # self.U -> N x D
+        # self.V[:, j] -> 1 x D
+        # np.dot(self.U, self.V[:, j]) -> N x 1
+        # aux_1 -> N x 1
+        # aux_2 -> N x D 
+        # sum(aux_2, axis = 1) -> 1 x D
         aux_1 = I_ij * (self.R[:, j] - np.dot(self.U, self.V[:, j]))
-        aux_2 = (aux_1 * self.U.T).T
+        aux_2 = aux_1 * self.U.T
         return (1 / self.sigma) * np.sum(aux_2, axis=1) + (self.V[:, j] / self.sigma_v)
